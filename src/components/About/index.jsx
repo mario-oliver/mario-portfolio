cf2aca684eafbc5609ead143f480f1f1bdc178f9
Mario Oliver <mar.mariooliver93@gmail.com>
2023-06-30 17:14:01 -0400

    Added about section header

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
 
 const About = () => {
-  return <div>index</div>;
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters strArray={'about me'.split('')} idx={15} />
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default About;
