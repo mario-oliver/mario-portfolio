2f26d4ef90f8dd2c32030c4a179088fd3872e5e9
Mario Oliver <mar.mariooliver93@gmail.com>
2023-06-30 16:29:01 -0400

    created a letter compononent that takes a string array and then animates each cahrater

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import { FaHandshake } from 'react-icons/fa';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['a', 'r', 'i', 'o', ' ', 'O', 'l', 'i', 'v', 'e', 'r'];
+  const jobArray = 'Full-Stack Web Developer'.split('');
+  console.log(jobArray);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi <FaHandshake /> <br />
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _10`}>i</span>
+          <span className={`${letterClass} _11`}> </span>
+          <span className={`${letterClass} _12`}>
+            <FaHandshake /> <br />
+          </span>
         </h1>
         <br />
         <h1>
-          I'm <img src={logo} alt="developer"></img>
-          ario Oliver
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={logo} alt="developer"></img>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
         </h1>
         <br />
         <h1>
-          Full-Stack Web Developer <br></br>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={26}
+          />{' '}
+          <br></br>
         </h1>
         <h2>
           Javascript & Java Expert | AWS Certified | Filmmaker | Cycling &
