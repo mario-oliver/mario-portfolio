d5c47e664e6f788e9995fc8110f2e7db74a28542
Mario Oliver <mar.mariooliver93@gmail.com>
2023-06-30 14:07:13 -0400

    added boilerplate html tags and outlet for rendered compoment within react router dom

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
@@ -5,8 +6,18 @@ import React from 'react';
 
 const Layout = () => {
   return (
-    <div>
+    <div className="App">
       <Sidebar />
+      <div className="page">
+        <span className="tag top-tags">&lt;body&gt;</span>
+        <Outlet></Outlet>
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   );
 };
